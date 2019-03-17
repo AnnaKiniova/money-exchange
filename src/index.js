d5418c05b5c1231034b4d4a719616168cc6009f3
Anna Kiniova <anna.kiniova@gmail.com>
2019-03-18 00:51:39 +0300

    feat: - add for cycle

--- src/index.js
+++ src/index.js
@@ -18,42 +18,20 @@ module.exports =function makeExchange(currency) {
     };
 
   
-
-    for (let key in Money) {
-        // for (key in change)
+    
+    for (key in Money) {
+       
         let calc = 0;
         calc = Math.floor(currency / Money[key]);
             if (calc != 0) 
             change[key] = calc; 
            
         currency = currency - (calc*Money[key]);
-        
     }
 
     return (change);
    
-    
-    // let H = Math.floor(+currency / 50);
-    //     if (H != 0) 
-    //     {
-    //         change.H = H;
-    //     }
-    // let Q = Math.floor((+currency % 50)/25);
-    //     if (Q != 0) 
-    //     {change.Q = Q;}
-    // let D=Math.floor((+currency % 50 % 25) / 10);
-    //     if (D != 0) 
-    //     {change.D = D;}
-    // let N=Math.floor((+currency % 50 % 25 % 10) / 5);
-    //     if (N != 0) 
-    //     {change.N = N;}
-    // let P=Math.floor((+currency % 50 % 25 % 10 % 5) / 1);
-    //     if (P != 0) 
-    //     {change.P = P;}
-
-    // return (change);
-
-   }
-  // makeExchange (135);
+}
+  
     
   
